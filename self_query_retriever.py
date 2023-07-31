66851c81fbf7e58ba0d79a098696b82093d15c47
Speaty <speatjack@gmail.com>
2023-07-31 09:59:49 +0100

    .

--- self_query_retriever.py
+++ self_query_retriever.py
@@ -8,21 +8,24 @@ import datetime
 
 import pinecone
 
-from langchain.schema import Document
 from langchain.document_loaders import DirectoryLoader, PyPDFLoader
-from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Pinecone
+from langchain.llms import OpenAI 
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
 
+# Save chat history
 def save_data(chat_history):
-    with open(chatlog + 'chat_history_self_query.txt', 'a') as f:
+    with open(chatlog + 'chat_history_self_query_v2.txt', 'a') as f:
         f.write('#' * 80)
         f.write('\n' + str(datetime.datetime.now()) + '\n')
         for item in chat_history:
-            print(item)
+            # print(item)
             f.write("Q: %s\n" % item[0])
             f.write("A: %s\n\n" % item[1])
         f.write('#' * 80 + '\n')
+
 
 ##############################################################################
 # Setup
@@ -63,8 +66,16 @@ elif setup == 'n':
     print("Loading index...")
     vectorstore = Pinecone.from_existing_index(index_name=keys.pinecone_index, embedding=embeddings)
 
+# Chat setup
+llm = OpenAI(temperature=0.9)
+prompt = PromptTemplate(
+    input_variables=["query"],
+    template = "Q: Can you briefly suggest a solution to this problem: \"{query}\" as a list of bullet points? \nA: ")
+chain = LLMChain(llm=llm, prompt=prompt)
+
 #query data
 history = []
+
 
 while True:
     query = None
@@ -73,10 +84,34 @@ while True:
     if query == 'exit':
         save_data(history)
         sys.exit()
+    
+    answer = chain.run(query)
+    print(answer, "\n\n")
+
     result = vectorstore.similarity_search_with_relevance_scores(query, k=3)
+
     for i in result:
-        print(i[0].metadata, '\nSCORE:\t', i[1], '\n\n')
+        if i[1] < 0.2:
+            print(i[0].metadata, '\nSCORE:\t', i[1], '\n\n')
+            
+        else:
+            print('No results found / low confidence\n\n')
 
-    history.append((query, result))
+    print("#" * 80, '\n\n')
+    history.append((query, answer, result))
 
+    
+    # result = vectorstore.similarity_search_with_relevance_scores(query, k=1)
+    # results = []
 
+    # for i in result:
+    #     if i[1] < 0.2:
+    #         print(i[0].metadata, '\nSCORE:\t', i[1], '\n\n')
+    #         answer = chain.run(i[0].page_content)
+    #         print(answer, "\n\n")
+    #         results.append((i[0].metadata, answer))
+    #     else:
+    #         print('No results found / low confidence\n\n')
+
+    # print("#" * 80, '\n\n')
+    # history.append((query, results))
